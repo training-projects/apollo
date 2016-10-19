2f9860ae48facdeb128395b05ddec90d558d9691
Luan Vicente <hi@idlua.me>
2016-10-19 01:21:26 -0200

    add gulpfile with all requires

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+// -----------------------------------------------------------------------------
+// My Precious
+// -----------------------------------------------------------------------------
+
+
+// RUNNERS (PLUGINS)
+var gulp         = require('gulp'),
+    plumber      = require('gulp-plumber'),
+    browserSync  = require('browser-sync'),
+    jade         = require('gulp-jade'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    stylus       = require('gulp-stylus'),
+    autoprefixer = require('autoprefixer-stylus'),
+    uglify       = require('gulp-uglify'),
+    imagemin     = require('gulp-imagemin');
