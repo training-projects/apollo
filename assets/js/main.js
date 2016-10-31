2685d70ad68aef9a026966f15ff63427fca8b537
Luan Vicente <hi@idlua.me>
2016-10-31 02:19:18 -0200

    scripts updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,2 @@
-for(var togglebutton=document.querySelectorAll(".togglebutton"),container=document.querySelector(".header-content"),i=0;i<togglebutton.length;i++)togglebutton[i].addEventListener("click",function(){container.classList.toggle("open")});
+for(var togglebutton=document.querySelectorAll(".togglebutton"),link=document.querySelectorAll(".link"),container=document.querySelector(".header-content"),i=0;i<togglebutton.length;i++)togglebutton[i].addEventListener("click",function(){container.classList.toggle("open")});for(var i=0;i<link.length;i++)link[i].addEventListener("click",function(){container.classList.toggle("open")});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 var togglebutton = document.querySelectorAll('.togglebutton'),
-    container     = document.querySelector('.header-content');
+    link         = document.querySelectorAll('.link'),
+    container    = document.querySelector('.header-content');
 
 for(var i = 0; i < togglebutton.length; i++) {
   togglebutton[i].addEventListener('click', function() {
+    container.classList.toggle('open');
+  });
+}
+
+for(var i = 0; i < link.length; i++) {
+  link[i].addEventListener('click', function() {
     container.classList.toggle('open');
   });
 }
